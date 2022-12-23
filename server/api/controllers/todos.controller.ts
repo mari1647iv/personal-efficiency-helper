9ba005e1160b62b55e4a7b3f8bfefab0f85c93cc
mari1647iv <ima1647@outlook.com>
2022-12-24 01:32:10 +0300

    post/delete todo routes

--- server/api/controllers/todos.controller.ts
+++ server/api/controllers/todos.controller.ts
@@ -16,37 +16,37 @@ const getTodos = async (req: Request, res: Response) => {
   }
 }
 
-// const createPlaylist = async (req: Request, res: Response) => {
-//   try {
-//     const { name, url, duration } = req.body
-//     await pool.query(insertPlaylistQuery, [name, url, duration])
-//     try {
-//       const playlists = await pool.query(getPlaylistsQuery)
-//       res.status(200).json(playlists.rows)
-//     } catch (getError: any) {
-//       console.error(`Error in getting playlists from db: ${getError}`)
-//       res.status(500).json({ ...getError, message: getError.toString() })
-//     }
-//   } catch (postError: any) {
-//     console.error(`Error in creating new playlist in db: ${postError}`)
-//     res.status(500).json({ ...postError, message: postError.toString() })
-//   }
-// }
+const createTodo = async (req: Request, res: Response) => {
+  try {
+    const { name, description, date, isImportant, state, type, userpoints } = req.body
+    await pool.query(insertTodoQuery, [name, description, date, isImportant, state, type, userpoints])
+    try {
+      const playlists = await pool.query(getTodosQuery)
+      res.status(200).json(playlists.rows)
+    } catch (getError: any) {
+      console.error(`Error in getting todos from db: ${getError}`)
+      res.status(500).json({ ...getError, message: getError.toString() })
+    }
+  } catch (postError: any) {
+    console.error(`Error in creating new todo in db: ${postError}`)
+    res.status(500).json({ ...postError, message: postError.toString() })
+  }
+}
 
-// const deletePlaylist = async (req: Request, res: Response) => {
-//   const id = parseInt(req.params.id)
+const deleteTodo = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
 
-//   try {
-//     await pool.query(deletePlaylistQuery, [id])
-//     res.status(200).json(`Playlist with id=${id} deleted`)
-//   } catch (err: any) {
-//     console.error(`Error in deliting playlist from db: ${err}`)
-//     res.status(500).json({ ...err, message: err.toString() })
-//   }
-// }
+  try {
+    await pool.query(deleteTodoQuery, [id])
+    res.status(200).json(`Todo with id=${id} deleted`)
+  } catch (err: any) {
+    console.error(`Error in deliting todo from db: ${err}`)
+    res.status(500).json({ ...err, message: err.toString() })
+  }
+}
 
 module.exports = {
   getTodos,
-  // createPlaylist,
-  // deletePlaylist,
+  createTodo,
+  deleteTodo,
 }
--- server/api/routes/todos.route.ts
+++ server/api/routes/todos.route.ts
@@ -4,7 +4,7 @@ const controller = require('../controllers/todos.controller')
 const router = Router()
 
 router.get('/', controller.getTodos);
-// router.post('/', controller.createPlaylist)
-// router.delete('/:id', controller.deletePlaylist)
+router.post('/', controller.createPlaylist)
+router.delete('/:id', controller.deletePlaylist)
 
 module.exports = router
